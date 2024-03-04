17517617f42598e1d3996808a0253b4f25d52edd
katik <kartikteja343@gmail.com>
2024-03-04 14:58:11 +0530

    fixing add tasks

--- smarter-tasks/src/TaskForm.tsx
+++ smarter-tasks/src/TaskForm.tsx
@@ -5,13 +5,17 @@ interface TaskFormProps {
     addTask: (task: TaskItem) => void;
 }
 interface TaskFormState {
-    title: string;
+    title: string,
+    description: string,
+    date: string,
 }
 class TaskForm extends React.Component<TaskFormProps, TaskFormState> {
     constructor(props: TaskFormProps) {
         super(props);
         this.state = {
-            title: ""
+            title: "",
+            description: "",
+            date: "",
         };
         this.addTask = this.addTask.bind(this);
     }
@@ -20,10 +24,20 @@ class TaskForm extends React.Component<TaskFormProps, TaskFormState> {
         this.setState({ title: event.target.value });
     };
 
+    descriptionChanged: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+        this.setState({ description: event.target.value });
+    };
+
+    dateChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+        this.setState({ date: event.target.value });
+    };
+
     addTask: React.FormEventHandler<HTMLFormElement> = (event) => {
         event.preventDefault();
         const newTask = {
             title: this.state.title,
+            description: this.state.description,
+            date: this.state.date,
         };
         this.props.addTask(newTask);
         this.setState({ title: "" });
@@ -31,8 +45,34 @@ class TaskForm extends React.Component<TaskFormProps, TaskFormState> {
     render() {
         return (
             <form onSubmit={this.addTask}>
-                <input className="border-solid border-2 p-2" type="text" value={this.state.title} onChange={this.titleChanged} />
-                <button type="submit" className="border-solid border-2 p-2">Add item</button>
+                <input
+                    id="todoTitle"
+                    className="border-solid border-2 p-2"
+                    type="text"
+                    value={this.state.title}
+                    onChange={this.titleChanged}
+                    required
+                />
+                <input
+                    id="todoDescription"
+                    type="text"
+                    value={this.state.description}
+                    onChange={this.descriptionChanged}
+                />
+                <input
+                    id="todoDueDate"
+                    type="date"
+                    value={this.state.date}
+                    onChange={this.dateChange}
+                    required
+                />
+                <button
+                    id="addTaskButton"
+                    type="submit"
+                    className="border-solid border-2 p-2"
+                >
+                    Add item
+                </button>
             </form>
         )
     }
