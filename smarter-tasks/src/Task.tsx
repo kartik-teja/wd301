055993b586d99e3a7e4fe41942732e3d8ceb2e32
kartik-teja <kartikteja343@gmail.com>
2024-03-05 15:16:15 +0530

    useStates

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -4,7 +4,7 @@ import "./TaskCard.css"
 interface TaskProp {
     title: string;
     description: string,
-    date: string,
+    dueDate: string,
 }
 
 class Task extends React.Component<TaskProp>{
@@ -13,7 +13,7 @@ class Task extends React.Component<TaskProp>{
             <div className="TaskItem shadow-md border border-slate-100">
                 <h2 className="text-base font-bold my-1">{this.props.title}</h2>
                 <p className="text-sm text-slate-500">
-                    Due Date:{this.props.date}
+                    Due Date:{this.props.dueDate}
                 </p>
                 <p className="text-sm text-slate-500">
                     Description:{this.props.description}
