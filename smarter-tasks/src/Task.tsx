1071fba64d661cdf2594352c37f5f04be7f60f02
kartik-teja <kartikteja343@gmail.com>
2024-03-06 21:42:00 +0530

    class to functions

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -1,26 +1,18 @@
-import React from "react";
+//import React from "react";
 import "./TaskCard.css"
+import { TaskItem } from "./types";
 
-interface TaskProp {
-    title: string;
-    description: string,
-    dueDate: string,
-}
-
-class Task extends React.Component<TaskProp>{
-    render() {
-        return (
-            <div className="TaskItem shadow-md border border-slate-100">
-                <h2 className="text-base font-bold my-1">{this.props.title}</h2>
-                <p className="text-sm text-slate-500">
-                    Due Date:{this.props.dueDate}
-                </p>
-                <p className="text-sm text-slate-500">
-                    Description:{this.props.description}
-                </p>
-            </div>
-        );
-    }
-}
+const Task = (props: TaskItem) => {
+    console.log(props);
+    return (
+        <div className="TaskItem shadow-md border border-slate-100">
+            <h2 className="text-base font-bold my-1">{props.title}</h2>
+            <p className="text-sm text-slate-500">dueDate: {props.dueDate}</p>
+            <p className="text-sm text-slate-500">
+                Description: {props.description}
+            </p>
+        </div>
+    );
+};
 
 export default Task;
