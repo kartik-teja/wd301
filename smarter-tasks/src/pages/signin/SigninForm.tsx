8117fc5011df22b80b05f9077d75b15057dc1930
kartik-teja <kartikteja343@gmail.com>
2024-03-21 12:56:31 +0530

    dashboard

--- smarter-tasks/src/pages/signin/SigninForm.tsx
+++ smarter-tasks/src/pages/signin/SigninForm.tsx
@@ -29,7 +29,7 @@ const SigninForm: React.FC = () => {
 
             localStorage.setItem('authToken', data.token);
             localStorage.setItem('userData', JSON.stringify(data.user));
-
+            navigate('/signin');
         } catch (error) {
             console.error('Sign-in failed:', error);
         }
