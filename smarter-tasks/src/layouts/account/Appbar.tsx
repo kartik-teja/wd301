2f285a8673a0b84f84f597d8589d8bd3417a67e4
kartik-teja <kartikteja343@gmail.com>
2024-03-24 18:13:50 +0530

    context

--- smarter-tasks/src/layouts/account/Appbar.tsx
+++ smarter-tasks/src/layouts/account/Appbar.tsx
@@ -1,8 +1,9 @@
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Fragment, useContext, useState } from 'react'
+import { Disclosure, Menu, Switch, Transition } from '@headlessui/react'
 import { UserCircleIcon } from '@heroicons/react/24/outline'
 import { Link, useLocation } from "react-router-dom"
 import Logo from '../../assets/images/logo.png'
+import { ThemeContext } from "../../context/theme";
 
 const userNavigation = [
     { name: 'Profile', href: '#' },
@@ -12,6 +13,20 @@ const userNavigation = [
 const classNames = (...classes: string[]): string => classes.filter(Boolean).join(' ');
 
 const Appbar = () => {
+    //const [enabled, setEnabled] = useState(false)
+    const { theme, setTheme } = useContext(ThemeContext)
+    const [enabled, setEnabled] = useState(theme === 'dark')
+
+    const toggleTheme = () => {
+        let newTheme = ''
+        if (theme === 'light') {
+            newTheme = 'dark'
+        } else {
+            newTheme = 'light'
+        }
+        setEnabled(!enabled)
+        setTheme(newTheme)
+    }
     const { pathname } = useLocation()
 
     const navigation = [
@@ -74,6 +89,25 @@ const Appbar = () => {
                                             leaveFrom="transform opacity-100 scale-100"
                                             leaveTo="transform opacity-0 scale-95"
                                         >
+                                            <Switch
+                                                checked={enabled}
+                                                onChange={toggleTheme}
+                                                className={`${enabled ? 'bg-slate-400' : 'bg-slate-700'}
+          relative inline-flex h-[24px] w-[60px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+                                            >
+                                                <span
+                                                    aria-hidden="true"
+                                                    className={`${enabled ? 'translate-x-9' : 'translate-x-0'}
+                pointer-events-none inline-block h-[16px] w-[16px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+                                                />
+                                            </Switch>
+                                            <Menu as="div" className="relative ml-3">
+                                                <div>
+                                                    <Menu.Button className="rounded-full bg-white p-1 text-gray-400 hover:text-blue-600">
+                                                        <UserCircleIcon className="h-6 w-6" aria-hidden="true" />
+                                                    </Menu.Button>
+                                                </div>
+                                            </Menu>
                                             <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                                                 {userNavigation.map((item) => (
                                                     <Menu.Item key={item.name}>
